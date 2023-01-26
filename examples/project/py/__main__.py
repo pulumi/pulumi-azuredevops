5b481607a02f4aaafa65626a923e8696c45b1b63
Anton Tayanovskyy <anton@pulumi.com>
2023-01-26 10:58:28 -0500

    Another attempt to improve entropy

--- examples/project/py/__main__.py
+++ examples/project/py/__main__.py
@@ -2,11 +2,8 @@ import pulumi
 import pulumi_random as random
 import pulumi_azuredevops as ado
 
-entropy = random.RandomId("entropy", byte_length=8)
+project_name = random.RandomId("demo-project-name", byte_length=8, prefix='demo-project-')
 
-project_name = random.RandomPet("demo-project-name", keepers={'entropy': entropy.hex})
-
-project = ado.Project("demo-project",
-                           name=project_name)
+project = ado.Project("demo-project", name=project_name.hex)
 
 pulumi.export("project_name", project.name)
--- examples/project/ts/index.ts
+++ examples/project/ts/index.ts
@@ -1,12 +1,10 @@
 import * as ado from "@pulumi/azuredevops";
 import * as random from "@pulumi/random";
 
-const entropy = new random.RandomId("entropy", {byteLength: 8});
-
-const randomProjectName = new random.RandomPet("demo-project-name", {keepers: {entropy: entropy.hex}});
+const randomProjectName = new random.RandomId("demo-project-name", {byteLength: 8, prefix: "demo-project-"});
 
 const project = new ado.Project("demo-project", {
-  name: randomProjectName.id,
+    name: randomProjectName.hex,
 });
 
 export const myProjectName = project.name;
