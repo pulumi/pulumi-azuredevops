2504c0124d1aa8c65614efda82ea650852d3d688
Anton Tayanovskyy <anton@pulumi.com>
2023-01-26 10:28:12 -0500

    Add entropy to examples to avoid racing for the same name

--- examples/project/py/__main__.py
+++ examples/project/py/__main__.py
@@ -2,11 +2,11 @@ import pulumi
 import pulumi_random as random
 import pulumi_azuredevops as ado
 
-project_name = random.RandomPet("demo-project-name")
+entropy = random.RandomId("entropy", byte_length=8)
+
+project_name = random.RandomPet("demo-project-name", keepers={'entropy': entropy.hex})
 
 project = ado.Project("demo-project",
                            name=project_name)
 
 pulumi.export("project_name", project.name)
-
-
--- examples/project/ts/index.ts
+++ examples/project/ts/index.ts
@@ -1,7 +1,9 @@
 import * as ado from "@pulumi/azuredevops";
 import * as random from "@pulumi/random";
 
-const randomProjectName = new random.RandomPet("demo-project-name");
+const entropy = new random.RandomId("entropy", {byteLength: 8});
+
+const randomProjectName = new random.RandomPet("demo-project-name", {keepers: {entropy: entropy.hex}});
 
 const project = new ado.Project("demo-project", {
   name: randomProjectName.id,
