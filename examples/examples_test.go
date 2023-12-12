6d5800986fbf5961c04834856eb699e46ed13730
VenelinMartinov <venelin.v.martinov@gmail.com>
2023-12-12 18:58:30 +0000

    Remove min reviewers defaults (#220)
    This is a targeted fix for
    https://github.com/pulumi/pulumi-azuredevops/issues/38 until we ship
    https://github.com/pulumi/pulumi-terraform-bridge/issues/1546.
    
    Similar to https://github.com/pulumi/pulumi-azuredevops/pull/219.

--- examples/examples_test.go
+++ examples/examples_test.go
@@ -13,6 +13,12 @@ import (
 	"github.com/pulumi/pulumi/pkg/v3/testing/integration"
 )
 
+func init() {
+	// This is necessary for gRPC testing. It doesn't effect integration tests, since
+	// they use their own binary.
+	version.Version = "6.0.0"
+}
+
 func getCwd(t *testing.T) string {
 	cwd, err := os.Getwd()
 	if err != nil {
@@ -25,12 +31,6 @@ func getBaseOptions(t *testing.T) integration.ProgramTestOptions {
 	return integration.ProgramTestOptions{
 		ExpectRefreshChanges: true,
 	}
-}
-
-func init() {
-	// This is necessary for gRPC testing. It doesn't effect integration tests, since
-	// they use their own binary.
-	version.Version = "6.0.0"
 }
 
 func replay(t *testing.T, sequence string) {
@@ -70,6 +70,77 @@ func TestServiceConnectionIdDefaultDoesNotConflict(t *testing.T) {
 					},
 					"name": "repo-779583a",
 					"projectId": "04da6b54-80e4-46f7-96ec-b56ff0331ba9"
+				}
+			},
+			"metadata": {
+				"kind": "resource",
+				"mode": "client",
+				"name": "azuredevops"
+			}
+		}
+  ]
+	`, "$", "`")
+	replay(t, repro)
+}
+
+// This test checks that the problem fixed in 0002-remove_min_reviwers_defaults.patch
+// does not fail the check for repo.
+// https://github.com/pulumi/pulumi-azuredevops/issues/227
+func TestMinReviewersDefaultDoesNotConflict(t *testing.T) {
+	repro := strings.ReplaceAll(`
+	[
+		{
+			"method": "/pulumirpc.ResourceProvider/Check",
+			"request": {
+				"urn": "urn:pulumi:dev::azure_devops_prog::azuredevops:index/branchPolicyMinReviewers:BranchPolicyMinReviewers::exampleBranchPolicyMinReviewers",
+				"olds": {},
+				"news": {
+					"projectId": "75e12870-434c-455b-b8ac-c0e17cc51c46",
+					"settings": {
+						"onPushResetAllVotes": true,
+						"reviewerCount": 7,
+						"scopes": [
+							{
+								"matchType": "Exact",
+								"repositoryId": "54745441-9448-43f9-a5cb-2c3644bbac54",
+								"repositoryRef": "refs/heads/master"
+							}
+						]
+					}
+				},
+				"randomSeed": "TxXqw2+y2hjh5dvv//yChyw2PYzaCeOdsx8RoYlqDBs="
+			},
+			"response": {
+				"inputs": {
+					"__defaults": [
+						"blocking",
+						"enabled"
+					],
+					"blocking": true,
+					"enabled": true,
+					"projectId": "75e12870-434c-455b-b8ac-c0e17cc51c46",
+					"settings": {
+						"__defaults": [
+							"allowCompletionWithRejectsOrWaits",
+							"lastPusherCannotApprove",
+							"onPushResetApprovedVotes",
+							"submitterCanVote"
+						],
+						"allowCompletionWithRejectsOrWaits": false,
+						"lastPusherCannotApprove": false,
+						"onPushResetAllVotes": true,
+						"onPushResetApprovedVotes": false,
+						"reviewerCount": 7,
+						"scopes": [
+							{
+								"__defaults": [],
+								"matchType": "Exact",
+								"repositoryId": "54745441-9448-43f9-a5cb-2c3644bbac54",
+								"repositoryRef": "refs/heads/master"
+							}
+						],
+						"submitterCanVote": false
+					}
 				}
 			},
 			"metadata": {
