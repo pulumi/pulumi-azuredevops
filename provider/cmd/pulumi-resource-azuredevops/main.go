8026baf1ed15f2249bdb9c7a96f144588b70d390
Ian Wahbe <ian@wahbe.com>
2024-01-10 11:54:14 -0800

    Fix java version (#263)

--- provider/cmd/pulumi-resource-azuredevops/main.go
+++ provider/cmd/pulumi-resource-azuredevops/main.go
@@ -12,15 +12,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:generate go run ./generate.go
-
 package main
 
 import (
+	_ "embed" // Allow embedding schema.json
+
 	azuredevops "github.com/pulumi/pulumi-azuredevops/provider/v2"
 	"github.com/pulumi/pulumi-azuredevops/provider/v2/pkg/version"
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
 )
+
+//go:embed schema.json
+var pulumiSchema []byte
 
 func main() {
 	// Modify the path to point to the new provider
