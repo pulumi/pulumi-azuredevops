8a80671e45c704b60b4a03c88efb00d19c036385
Ian Wahbe <ian@wahbe.com>
2024-03-12 11:23:18 +0100

    Upgrade to v3.0.0

--- provider/go.mod
+++ provider/go.mod
@@ -1,4 +1,4 @@
-module github.com/pulumi/pulumi-azuredevops/provider/v2
+module github.com/pulumi/pulumi-azuredevops/provider/v3
 
 go 1.21
 
--- provider/resources.go
+++ provider/resources.go
@@ -28,7 +28,7 @@ import (
 	shimv2 "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim/sdk-v2"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/tokens"
 
-	"github.com/pulumi/pulumi-azuredevops/provider/v2/pkg/version"
+	"github.com/pulumi/pulumi-azuredevops/provider/v3/pkg/version"
 )
 
 // all of the token components used below.
